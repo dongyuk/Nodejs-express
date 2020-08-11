589484d8ed8709683c1be6c489434a2129f83d55
dongyuk <dykim3344@naver.com>
2020-08-11 19:15:24 +0900

    commit

--- ReadMe.md
+++ ReadMe.md
@@ -1,3 +1,14 @@
 # Express
+##
+### route, routing
+- 갈림길에서 적당한곳에섯 자리를 잡는 것.
+- 사용자들이 여러가지 path로 들어올때 알맞은 응답을 해줌.
 
-###
+### url path 를 통해 파라미터를 전달하는 경우 express에서 처리하는 케이스.
+```
+app.get('/page/:pageId', function(request, response) {
+  // clinet url path : localhost:3000/page/HTML 
+  // request.params = {"pageId","HTML"}
+  response.send(request.params);
+});
+```
--- main.js
+++ main.js
@@ -20,8 +20,11 @@ app.get('/', function(request, response) {
   });
 });
 
-app.get('/page', function(req, res) {
-  return res.send('/page');
+// url path 를 통해 파라미터를 전달하는 경우 express에서 처리하는 케이스.
+app.get('/page/:pageId', function(request, response) {
+  // clinet url path : localhost:3000/page/HTML 
+  // request.params = {"pageId","HTML"}
+  response.send(request.params);
 });
 
 
